84adbd9755094567b1bd182f232d059e3e8d3aa3
yangwc2000 <yangwoochang@naver.com>
2025-02-20 17:17:20 +0900

    NXGameMode.h 파일 수정

--- Source/NXProject/Game/NXGameMode.h
+++ Source/NXProject/Game/NXGameMode.h
@@ -3,14 +3,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/GameModeBase.h"
+#include "GameFramework/GameMode.h"
 #include "NXGameMode.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class NXPROJECT_API ANXGameMode : public AGameModeBase
+class NXPROJECT_API ANXGameMode : public AGameMode
 {
 	GENERATED_BODY()
 	
