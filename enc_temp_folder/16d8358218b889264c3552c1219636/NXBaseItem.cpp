3a13723fc07e8c3858c5d51320830e32c3bcebe8
Wonjihui <jt3624@naver.com>
2025-02-24 00:18:49 +0900

    WJH_TestMap/ Blueprint 추가

---
+++ enc_temp_folder/16d8358218b889264c3552c1219636/NXBaseItem.cpp
@@ -0,0 +1,64 @@
+#include "NXBaseItem.h"
+#include "Components/SphereComponent.h"
+
+ANXBaseItem::ANXBaseItem()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+    Scene = CreateDefaultSubobject<USceneComponent>(TEXT("Scene"));
+    SetRootComponent(Scene);
+
+
+    Collision = CreateDefaultSubobject<USphereComponent>(TEXT("Collision"));
+
+    Collision->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
+
+    Collision->SetupAttachment(Scene);
+
+    StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+    StaticMesh->SetupAttachment(Collision);
+
+    Collision->OnComponentBeginOverlap.AddDynamic(this, &ANXBaseItem::OnItemOverlap);
+    Collision->OnComponentEndOverlap.AddDynamic(this, &ANXBaseItem::OnItemEndOverlap);
+
+}
+
+void ANXBaseItem::OnItemOverlap(
+	UPrimitiveComponent* OverlappedComp,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex,
+	bool bFromSweep,
+	const FHitResult& SweepResult)
+{
+	
+	if (OtherActor && OtherActor->ActorHasTag("Player"))
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Green, FString::Printf(TEXT("Overlap!!!")));
+		
+		ActivateItem(OtherActor);
+	}
+}
+
+void ANXBaseItem::OnItemEndOverlap(
+	UPrimitiveComponent* OverlappedComp,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex)
+{
+}
+
+void ANXBaseItem::ActivateItem(AActor* Activator)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Green, FString::Printf(TEXT("Overlap!!")));
+}
+
+FName ANXBaseItem::GetItemType() const
+{
+	return ItemType;
+}
+
+void ANXBaseItem::DestroyItem()
+{
+	Destroy();
+}
