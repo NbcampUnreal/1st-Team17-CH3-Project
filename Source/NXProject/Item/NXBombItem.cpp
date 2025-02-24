7db7e896766c71976c01c98d762472c9cdd276ca
Wonjihui <jt3624@naver.com>
2025-02-25 00:11:31 +0900

    NXBombItem 수정

--- Source/NXProject/Item/NXBombItem.cpp
+++ Source/NXProject/Item/NXBombItem.cpp
@@ -1,5 +1,86 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 
 #include "Item/NXBombItem.h"
 
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Engine/Engine.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/DamageType.h"
+
+ANXBombItem::ANXBombItem()
+{
+   
+    ExplosionDelay = 3.0f;
+    ExplosionRadius = 250.f;
+    ExplosionDamage = 30.f;
+    ItemType = "Mine";
+
+
+    MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+    MeshComp->SetupAttachment(Scene);  
+
+   
+    ExplosionCollision = CreateDefaultSubobject<USphereComponent>(TEXT("ExplosionCollision"));
+    ExplosionCollision->InitSphereRadius(ExplosionRadius);
+    ExplosionCollision->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
+    ExplosionCollision->SetupAttachment(Scene);
+
+
+   //아이템 던지기
+    ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement"));
+    ProjectileMovement->UpdatedComponent = Scene;
+    ProjectileMovement->InitialSpeed = 100.f;
+    ProjectileMovement->MaxSpeed = 100.f;
+    ProjectileMovement->bRotationFollowsVelocity = true;
+    ProjectileMovement->ProjectileGravityScale = 0.f;
+   
+}
+
+void ANXBombItem::ThrowItem(const FVector& LaunchVelocity)
+{
+    if (ProjectileMovement)
+    {
+        ProjectileMovement->Velocity = LaunchVelocity;
+    }
+}
+
+void ANXBombItem::ActivateItem(AActor* Activator)
+{
+
+    GetWorld()->GetTimerManager().SetTimer(ExplosionTimerHandle, this, &ANXBombItem::Explode, ExplosionDelay, false);
+
+    if (GEngine)
+    {
+        GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Yellow,
+            FString::Printf(TEXT("Mine activated! Exploding in %f seconds"), ExplosionDelay));
+    }
+}
+
+void ANXBombItem::Explode()
+{
+    TArray<AActor*> OverlappingActors;
+    ExplosionCollision->GetOverlappingActors(OverlappingActors);
+
+    for (AActor* Actor : OverlappingActors)
+    {
+        if (Actor && Actor->ActorHasTag("NonPlayer"))
+        {
+          
+            UGameplayStatics::ApplyDamage(
+                Actor,
+                ExplosionDamage,
+                nullptr,
+                this,
+                UDamageType::StaticClass()
+            );
+
+            if (GEngine)
+            {
+                GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Red,
+                    FString::Printf(TEXT("NonPlayer damaged %d by MineItem"), (int)ExplosionDamage));
+            }
+        }
+    }
+    DestroyItem();
+}
--- Source/NXProject/Item/NXBombItem.h
+++ Source/NXProject/Item/NXBombItem.h
@@ -1,4 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -6,12 +5,44 @@
 #include "Item/NXBaseItem.h"
 #include "NXBombItem.generated.h"
 
-/**
- * 
- */
+class USphereComponent;
+class UStaticMeshComponent;
+class UProjectileMovementComponent;
+
 UCLASS()
 class NXPROJECT_API ANXBombItem : public ANXBaseItem
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+public:
+    
+    ANXBombItem();
+
+    UFUNCTION(BlueprintCallable, Category = "Item")
+    void ThrowItem(const FVector& LaunchVelocity);
+
+protected:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Mine|Component")
+    UStaticMeshComponent* MeshComp;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Mine|Component")
+    USphereComponent* ExplosionCollision;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mine")
+    float ExplosionDelay;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mine")
+    float ExplosionRadius;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mine")
+    float ExplosionDamage;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+    UProjectileMovementComponent* ProjectileMovement;
+
+    FTimerHandle ExplosionTimerHandle;
+
+    virtual void ActivateItem(AActor* Activator) override;
+
+    void Explode();
 };
