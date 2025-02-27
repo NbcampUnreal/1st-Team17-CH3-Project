a7db29b54a7f2a63b4890698fa30bfcaf08b2845
Wonjihui <jt3624@naver.com>
2025-02-27 17:22:54 +0900

    교실 문

--- Source/NXProject/Item/NXDoor.cpp
+++ Source/NXProject/Item/NXDoor.cpp
@@ -1,22 +1,108 @@
 #include "NXDoor.h"
+#include "Components/BoxComponent.h"
 #include "Components/StaticMeshComponent.h"
+#include "Components/TimelineComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/Engine.h"
+#include "TimerManager.h"
 
 ANXDoor::ANXDoor()
 {
-    PrimaryActorTick.bCanEverTick = false;
+    PrimaryActorTick.bCanEverTick = true;
+    bIsOpen = false;
+    TargetOffset = FVector(0.f, 2000.f, 0.f); 
+
+    DoorTrigger = CreateDefaultSubobject<UBoxComponent>(TEXT("DoorTrigger"));
+    RootComponent = DoorTrigger;
+    DoorTrigger->SetBoxExtent(FVector(100.f, 100.f, 150.f));
+    DoorTrigger->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    DoorTrigger->SetCollisionObjectType(ECollisionChannel::ECC_WorldDynamic);
+    DoorTrigger->SetCollisionResponseToAllChannels(ECR_Ignore);
+    DoorTrigger->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+    DoorTrigger->SetGenerateOverlapEvents(true);
+    DoorTrigger->OnComponentBeginOverlap.AddDynamic(this, &ANXDoor::OnOverlap);
+
 
     DoorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorMesh"));
-    RootComponent = DoorMesh;
+    DoorMesh->SetupAttachment(RootComponent);
+
+    OpenDoorTimeline = CreateDefaultSubobject<UTimelineComponent>(TEXT("OpenDoorTimeline"));
 }
 
 void ANXDoor::BeginPlay()
 {
     Super::BeginPlay();
+
+    ClosedLocation = GetActorLocation();
+
+    if (OpenCurve)
+    {
+        FOnTimelineFloat TimelineCallback;
+        TimelineCallback.BindUFunction(this, FName("HandleDoorMovement"));
+        OpenDoorTimeline->AddInterpFloat(OpenCurve, TimelineCallback);
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("OpenCurve is not set in %s"), *GetName());
+    }
 }
+
+void ANXDoor::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    if (OpenDoorTimeline)
+    {
+        OpenDoorTimeline->TickComponent(DeltaTime, ELevelTick::LEVELTICK_TimeOnly, nullptr);
+    }
+}
+
+void ANXDoor::HandleDoorMovement(float Value)
+{
+
+    FVector NewLocation = FMath::Lerp(ClosedLocation, ClosedLocation + TargetOffset, Value);
+    SetActorLocation(NewLocation);
+}
+
 
 void ANXDoor::OpenDoor()
 {
-    FVector NewLocation = GetActorLocation();
-    NewLocation.Z += 200.0f;
-    SetActorLocation(NewLocation);
+    if (!bIsOpen && OpenCurve)
+    {
+        bIsOpen = true;
+        OpenDoorTimeline->PlayFromStart();
+
+        if (GEngine)
+        {
+            GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Green, TEXT("Door Opened!"));
+        }
+        UE_LOG(LogTemp, Warning, TEXT("Door Opened! Moving from %s to %s"), *ClosedLocation.ToString(), *(ClosedLocation + TargetOffset).ToString());
+    }
+    else if (bIsOpen)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Door is already open."));
+    }
+}
+
+void ANXDoor::OnOverlap(
+    UPrimitiveComponent* OverlappedComponent,
+    AActor* OtherActor,
+    UPrimitiveComponent* OtherComp,
+    int32 OtherBodyIndex,
+    bool bFromSweep,
+    const FHitResult& SweepResult
+)
+{
+    if (!OtherActor)
+        return;
+
+    UE_LOG(LogTemp, Warning, TEXT("Door Trigger Overlap with: %s"), *OtherActor->GetName());
+
+    
+    if (OtherActor->ActorHasTag("Player"))
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Player detected in door trigger. Opening door in 1 seconds."));
+
+        GetWorld()->GetTimerManager().SetTimer(DoorOpenDelayTimerHandle, this, &ANXDoor::OpenDoor, 1.0f, false);
+    }
 }
--- Source/NXProject/Item/NXDoor.h
+++ Source/NXProject/Item/NXDoor.h
@@ -4,6 +4,11 @@
 #include "GameFramework/Actor.h"
 #include "NXDoor.generated.h"
 
+class UBoxComponent;
+class UStaticMeshComponent;
+class UTimelineComponent;
+class UCurveFloat;
+
 UCLASS()
 class NXPROJECT_API ANXDoor : public AActor
 {
@@ -11,12 +16,59 @@ class NXPROJECT_API ANXDoor : public AActor
 
 public:
     ANXDoor();
-    void OpenDoor();
 
 protected:
     virtual void BeginPlay() override;
+    virtual void Tick(float DeltaTime) override;
+
+    UFUNCTION()
+    void HandleDoorMovement(float Value);
+
+
+    UFUNCTION()
+    void OnOverlap(
+        UPrimitiveComponent* OverlappedComponent,
+        AActor* OtherActor,
+        UPrimitiveComponent* OtherComp,
+        int32 OtherBodyIndex,
+        bool bFromSweep,
+        const FHitResult& SweepResult
+    );
 
 public:
-    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Door")
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Door")
+    UBoxComponent* DoorTrigger;
+
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Door")
     UStaticMeshComponent* DoorMesh;
+
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Door")
+    UTimelineComponent* OpenDoorTimeline;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Door|Animation")
+    UCurveFloat* OpenCurve;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Door|Animation")
+    FVector TargetOffset;
+
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Door")
+    bool bIsOpen;
+
+
+    UFUNCTION(BlueprintCallable, Category = "Door")
+    void OpenDoor();
+
+
+
+    FTimerHandle DoorOpenDelayTimerHandle;
+
+private:
+
+    FVector ClosedLocation;
+
+
 };
