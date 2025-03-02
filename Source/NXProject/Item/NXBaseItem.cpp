ebda71e023977b4a36f7c2464b192c0beb9401c4
Wonjihui <jt3624@naver.com>
2025-03-02 16:44:52 +0900

    아이템 이펙트

--- Source/NXProject/Item/NXBaseItem.cpp
+++ Source/NXProject/Item/NXBaseItem.cpp
@@ -54,40 +54,7 @@ void ANXBaseItem::OnItemEndOverlap(
 
 void ANXBaseItem::ActivateItem(AActor* Activator)
 {
-    GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Green, TEXT("Overlap!!"));
-
-    UNiagaraComponent* NiagaraComponent = nullptr;
-
-
-
-    if (PickupNiagara)
-    {
-        NiagaraComponent = UNiagaraFunctionLibrary::SpawnSystemAtLocation(
-            GetWorld(),
-            PickupNiagara,
-            GetActorLocation(),
-            GetActorRotation(),
-            FVector(1.0f),   
-            false,          
-            true,           
-            ENCPoolMethod::None
-        );
-    }
-
- 
-    if (NiagaraComponent)
-    {
-        FTimerHandle DestroyNiagaraTimerHandle;
-        GetWorld()->GetTimerManager().SetTimer(
-            DestroyNiagaraTimerHandle,
-            [NiagaraComponent]()
-            {
-                NiagaraComponent->DestroyComponent();
-            },
-            2.0f,
-            false
-        );
-    }
+   
 }
 
 FName ANXBaseItem::GetItemType() const
